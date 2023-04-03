9118415f4a5dc9d09c2cca3d73f6b749fbe92d4a
Roy Bruschini <r.bruschini@key2.it>
2023-04-03 18:24:45 +0200

    Added new configuration

--- .config/i3/scripts/managemonitor.sh
+++ .config/i3/scripts/managemonitor.sh
@@ -1,14 +1,15 @@
 #!/bin/zsh
 
-if xrandr | grep -w 'HDMI-2 connected'; then 
+if xrandr | grep -w 'HDMI-1 connected'; then 
 	if xrandr | grep -w 'DP-1 connected'; then
-		xrandr --output eDP-1 --off --output HDMI-2 --primary --mode 2560x1080 --rate 75.00 --output DP-1 --rotate right --right-of HDMI-2
+		xrandr --output eDP-1 --off --output HDMI-1 --primary --mode 2560x1080 --rate 75.00 --output DP-1 --rotate right --right-of HDMI-1
 		nitrogen --set-auto ~/Pictures/desktop1.jpg --head=0
-		nitrogen --set-auto ~/pictures/desktop2.jpg --head=1 
+		nitrogen --set-auto ~/Pictures/desktop2.jpg --head=1 
 	else 
-		xrandr --output eDP-1 --off --output HDMI-2 --primary --mode 2560x1080 --rate 75.00       	
+		xrandr --output eDP-1 --off --output HDMI-1 --primary --mode 2560x1080 --rate 75.00       	
 		nitrogen --set-auto ~/Pictures/desktophd.jpg --head=0
 	fi
 else
+    xrandr --output eDP-1 --primary
     nitrogen --set-auto ~/Pictures/desktop1.jpg --head=0
 fi	
